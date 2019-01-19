7d1ee516e0657baa9ce7815bb8a31669ec66ac0e
suhaas-livcd <suhaas95@gmail.com>
2019-01-19 11:36:35 +0530

    Added colors
    Signed-off-by: suhaas-livcd <suhaas95@gmail.com>

--- app/src/main/java/com/example/idlikadai/bulbula/MainActivity.java
+++ app/src/main/java/com/example/idlikadai/bulbula/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.idlikadai.bulbula;
 
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
@@ -10,7 +10,8 @@ import android.view.animation.TranslateAnimation;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView mIntroTextView_intro_1 = null,
+    private TextView
+            mIntroTextView_intro_1 = null,
             mIntroTextView_intro_2 = null,
             mIntroTextView_intro_3 = null;
 
