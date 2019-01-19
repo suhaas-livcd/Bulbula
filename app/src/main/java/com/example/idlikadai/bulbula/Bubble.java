b562a44001785534360a9f247aefd1fdeb4aa6a0
suhaas-livcd <suhaas95@gmail.com>
2019-01-19 23:12:20 +0530

    Ported Code and fixed minor issues
    Signed-off-by: suhaas-livcd <suhaas95@gmail.com>

---
+++ app/src/main/java/com/example/idlikadai/bulbula/Bubble.java
@@ -0,0 +1,107 @@
+package com.example.idlikadai.bulbula;
+
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
+
+import com.example.idlikadai.bulbula.utils.PixelHelper;
+
+/**
+ * Created by suhaas on 19/1/19.
+ */
+
+public class Bubble extends android.support.v7.widget.AppCompatImageView implements Animator.AnimatorListener, ValueAnimator.AnimatorUpdateListener {
+
+    private ValueAnimator mAnimator;
+    private BubbleListener mListener;
+    private boolean mPopped;
+
+    public Bubble(Context context) {
+        super(context);
+    }
+
+    public Bubble(Context context, int color, int rawHeight) {
+        super(context);
+
+        mListener = (BubbleListener) context;
+
+        this.setImageResource(R.drawable.ic_star);
+        this.setColorFilter(color);
+
+        int rawWidth = rawHeight / 2;
+
+        int dpHeight = PixelHelper.pixelsToDp(rawHeight, context);
+        int dpWidth = PixelHelper.pixelsToDp(rawWidth, context);
+
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(dpWidth, dpHeight);
+        this.setLayoutParams(params);
+
+    }
+
+    public void releaseBubble(int screenHeight, int duration) {
+
+        mAnimator = new ValueAnimator();
+        mAnimator.setDuration(duration);
+        mAnimator.setFloatValues(screenHeight, 0f);
+        mAnimator.setInterpolator(new LinearInterpolator());
+        mAnimator.setTarget(this);
+        mAnimator.addListener(this);
+        mAnimator.addUpdateListener(this);
+        mAnimator.start();
+    }
+
+    @Override
+    public void onAnimationStart(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animator) {
+        if (!mPopped) {
+            mListener.popBubble(this, false);
+        }
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animator) {
+        if (!mPopped) {
+            mListener.popBubble(this, false);
+        }
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+        setY((Float) valueAnimator.getAnimatedValue());
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (!mPopped && event.getAction() == MotionEvent.ACTION_DOWN) {
+            mListener.popBubble(this, true);
+            mPopped = true;
+            mAnimator.cancel();
+        }
+
+//        return super.onTouchEvent(event);
+        return true;
+    }
+
+    public void setPopped(boolean popped) {
+        mPopped = popped;
+        if (popped) {
+            mAnimator.cancel();
+        }
+    }
+
+    public interface BubbleListener {
+        void popBubble(Bubble bubble, boolean userTouch);
+    }
+}
--- app/src/main/java/com/example/idlikadai/bulbula/StartActivity.java
+++ app/src/main/java/com/example/idlikadai/bulbula/StartActivity.java
@@ -1,13 +1,300 @@
 package com.example.idlikadai.bulbula;
 
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class StartActivity extends AppCompatActivity {
+import com.example.idlikadai.bulbula.utils.HighScoreHelper;
+import com.example.idlikadai.bulbula.utils.SimpleAlertDialog;
+import com.example.idlikadai.bulbula.utils.SoundHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class StartActivity extends AppCompatActivity implements Bubble.BubbleListener {
+
+    private static final int MIN_ANIMATION_DELAY = 500;
+    private static final int MAX_ANIMATION_DELAY = 1500;
+    private static final int MIN_ANIMATION_DURATION = 1000;
+    private static final int MAX_ANIMATION_DURATION = 8000;
+    private static final int NUMBER_OF_PINS = 5;
+    private static final int BUBBLE_PER_LEVEL = 10;
+
+    private ViewGroup mContentView;
+    private int[] mBubbleColors = new int[3];
+    private int mNextColor, mScreenWidth, mScreenHeight;
+    TextView mScoreDisplay, mLevelDisplay;
+    private List<ImageView> mPinImages = new ArrayList<>();
+    private List<Bubble> mBubbles = new ArrayList<>();
+    Button mGoButton;
+
+    private boolean mPlaying;
+    private boolean mGameStopped = true;
+    private int mLevel, mScore, mPinsUsed;
+    private int mBubblesPopped;
+
+    private SoundHelper mSoundHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        mBubbleColors[0] = Color.argb(255, 255, 0, 0);
+        mBubbleColors[1] = Color.argb(255, 0, 255, 0);
+        mBubbleColors[2] = Color.argb(255, 0, 0, 255);
+
+//        getWindow().setBackgroundDrawableResource(R.drawable.bubble_3);
+
+        mContentView = (ViewGroup) findViewById(R.id.startActivityMainLayout);
+        setToFullScreen();
+
+        ViewTreeObserver viewTreeObserver = mContentView.getViewTreeObserver();
+        if (viewTreeObserver.isAlive()) {
+            viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                @Override
+                public void onGlobalLayout() {
+                    mContentView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                    mScreenHeight = mContentView.getHeight();
+                    mScreenWidth = mContentView.getWidth();
+                }
+            });
+        }
+
+        mContentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setToFullScreen();
+            }
+        });
+
+        mScoreDisplay = (TextView) findViewById(R.id.textView_gameScore);
+        mLevelDisplay = (TextView) findViewById(R.id.textView_gameLevel);
+        mPinImages.add((ImageView) findViewById(R.id.health_100));
+        mPinImages.add((ImageView) findViewById(R.id.health_90));
+        mPinImages.add((ImageView) findViewById(R.id.health_80));
+        mPinImages.add((ImageView) findViewById(R.id.health_70));
+        mPinImages.add((ImageView) findViewById(R.id.health_60));
+        mPinImages.add((ImageView) findViewById(R.id.health_50));
+        mPinImages.add((ImageView) findViewById(R.id.health_40));
+        mPinImages.add((ImageView) findViewById(R.id.health_30));
+        mPinImages.add((ImageView) findViewById(R.id.health_20));
+        mGoButton = (Button) findViewById(R.id.settings);
+
+        updateDisplay();
+
+        mSoundHelper = new SoundHelper(this);
+        mSoundHelper.prepareMusicPlayer(this);
+
+
+    }
+
+    private void setToFullScreen() {
+
+        ViewGroup rootLayout = (ViewGroup) findViewById(R.id.startActivityMainLayout);
+
+        rootLayout.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        );
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setToFullScreen();
+    }
+
+    private void startGame() {
+        setToFullScreen();
+        mScore = 0;
+        mLevel = 0;
+        mPinsUsed = 0;
+
+        for (ImageView pin : mPinImages) {
+            pin.setImageResource(R.drawable.ic_cards_heart);
+        }
+
+        mGameStopped = false;
+        startLevel();
+        mSoundHelper.playMusic();
+    }
+
+    private void startLevel() {
+        mLevel++;
+        updateDisplay();
+        BubbleLauncher bubbleLauncher = new BubbleLauncher();
+        bubbleLauncher.execute(mLevel);
+        mPlaying = true;
+        mBubblesPopped = 0;
+        mGoButton.setText("Stop Game");
+    }
+
+    private void finishLevel() {
+        Toast.makeText(this, String.format("You finished level %d", mLevel), Toast.LENGTH_SHORT)
+                .show();
+        mPlaying = false;
+        mGoButton.setText(String.format("Start level %d", mLevel + 1));
+    }
+
+    public void goButtonClickHandler(View view) {
+        if (mPlaying) {
+            gameOver(false);
+        } else if (mGameStopped) {
+            startGame();
+        } else {
+            startLevel();
+        }
+    }
+
+    @Override
+    public void popBubble(Bubble bubble, boolean userTouch) {
+
+        mSoundHelper.playSound();
+
+        mBubblesPopped++;
+
+        mContentView.removeView(bubble);
+        mBubbles.remove(bubble);
+
+        if (userTouch) {
+            mScore++;
+        } else {
+            mPinsUsed++;
+            if (mPinsUsed <= mPinImages.size()) {
+                mPinImages.get(mPinsUsed - 1)
+                        .setImageResource(R.drawable.ic_heart_off);
+            }
+            if (mPinsUsed == NUMBER_OF_PINS) {
+                gameOver(true);
+                return;
+            } else {
+                Toast.makeText(this, "Missed that one!", Toast.LENGTH_SHORT).show();
+            }
+        }
+        updateDisplay();
+
+        if (mBubblesPopped == BUBBLE_PER_LEVEL) {
+            finishLevel();
+        }
+    }
+
+
+    private void gameOver(boolean allPinsUsed) {
+        mSoundHelper.pauseMusic();
+        Toast.makeText(this, "Game Over!", Toast.LENGTH_SHORT).show();
+
+        for (Bubble bubble : mBubbles) {
+            mContentView.removeView(bubble);
+            bubble.setPopped(true);
+        }
+
+        mBubbles.clear();
+        mPlaying = false;
+        mGameStopped = true;
+        mGoButton.setText("Start Game");
+
+        if (allPinsUsed) {
+            if (HighScoreHelper.isTopScore(this, mScore)) {
+                HighScoreHelper.setTopScore(this, mScore);
+
+                SimpleAlertDialog dialog = SimpleAlertDialog.newInstance("New High Score!",
+                        String.format("Your new high score is %d", mScore));
+                dialog.show(getSupportFragmentManager(), null);
+            }
+        }
+    }
+
+    private void updateDisplay() {
+        mScoreDisplay.setText(String.valueOf(mScore));
+        mLevelDisplay.setText(String.valueOf(mLevel));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        gameOver(false);
+    }
+
+
+    private class BubbleLauncher extends AsyncTask<Integer, Integer, Void> {
+
+        @Override
+        protected Void doInBackground(Integer... params) {
+
+            if (params.length != 1) {
+                throw new AssertionError(
+                        "Expected 1 param for current level");
+            }
+
+            int level = params[0];
+            int maxDelay = Math.max(MIN_ANIMATION_DELAY,
+                    (MAX_ANIMATION_DELAY - ((level - 1) * 500)));
+            int minDelay = maxDelay / 2;
+
+            int bubblesLaunched = 0;
+            while (mPlaying && bubblesLaunched < BUBBLE_PER_LEVEL) {
+
+//              Get a random horizontal position for the next bubble
+                Random random = new Random();
+                int xPosition = random.nextInt(mScreenWidth - 200);
+                publishProgress(xPosition);
+                bubblesLaunched++;
+
+//              Wait a random number of milliseconds before looping
+                int delay = random.nextInt(minDelay) + minDelay;
+                try {
+                    Thread.sleep(delay);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            int xPosition = values[0];
+            launchBubble(xPosition);
+        }
+
+    }
+
+    private void launchBubble(int x) {
+
+        Bubble bubble = new Bubble(this, mBubbleColors[mNextColor], 150);
+        mBubbles.add(bubble);
+
+        if (mNextColor + 1 == mBubbleColors.length) {
+            mNextColor = 0;
+        } else {
+            mNextColor++;
+        }
+
+//      Set bubble vertical position and dimensions, add to container
+        bubble.setX(x);
+        bubble.setY(mScreenHeight + bubble.getHeight());
+        mContentView.addView(bubble);
+
+//      Let 'er fly
+        int duration = Math.max(MIN_ANIMATION_DURATION, MAX_ANIMATION_DURATION - (mLevel * 1000));
+        bubble.releaseBubble(mScreenHeight, duration);
+
     }
 }
